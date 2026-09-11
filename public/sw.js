0a97e983d2286cccb192960f0e778100130f4e04
intcodeug-prog <intcodeug@gmail.com>
2026-09-11 09:36:13 +0300

    fix: invalidate stale KROWN branding cache

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'krown-erp-v2';
-const ASSETS_TO_CACHE = ['/', '/manifest.json', '/icon.svg'];
+const CACHE_NAME = 'krown-erp-v3-branding';
+const ASSETS_TO_CACHE = ['/','/manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
