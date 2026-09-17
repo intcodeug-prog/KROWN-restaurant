59c2268c56e929604e5e349d93c181b53f1f297d
intcodeug-prog <intcodeug@gmail.com>
2026-09-17 13:07:07 +0300

    feat: add printer testing lab page

---
+++ app/printing-test/page.tsx
@@ -0,0 +1,5 @@
+import PrinterManager from '@/components/printer-manager';
+
+export default function PrintingTestPage() {
+  return <PrinterManager organizationId="printing-test" branchId="printing-test-branch" />;
+}
